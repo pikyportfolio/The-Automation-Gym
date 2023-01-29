c49a2241f09455b1fb60a3a0cc739eb45d0a4dd9
niko <nikolaynaydenovuk@gmail.com>
2023-01-29 21:21:11 +0000

    button changes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const subscribeButton = document.querySelector('form[name="subscribe"] button');
-subscribeButton.addEventListener('click', () => {
-  const form = document.querySelector('form[name="subscribe"]');
-  form.setAttribute('data-netlify-recaptcha', true);
+const submitButton = document.querySelector('form[name="subscribe"] [type="submit"]');
+submitButton.addEventListener('click', () => {
+const form = document.querySelector('form[name="subscribe"]');
+form.setAttribute('data-netlify-recaptcha', true);
 });
