9e82c14e32206872bbe6f340de553841e5ece175
niko <nikolaynaydenovuk@gmail.com>
2023-01-29 21:27:22 +0000

    no captcha headache

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const submitButton = document.querySelector('form[name="subscribe"] [type="submit"]');
-submitButton.addEventListener('click', () => {
-const form = document.querySelector('form[name="subscribe"]');
-form.setAttribute('data-netlify-recaptcha', true);
-});
+// const submitButton = document.querySelector('form[name="subscribe"] [type="submit"]');
+// submitButton.addEventListener('click', () => {
+// const form = document.querySelector('form[name="subscribe"]');
+// form.setAttribute('data-netlify-recaptcha', true);
+// });
